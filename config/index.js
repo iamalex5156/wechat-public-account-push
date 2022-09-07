39ace41e3969726bbd1a6c1459bebb1a6e9b5c50
Alex Xu <75003875+iamalex5156@users.noreply.github.com>
2022-09-07 22:48:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ogLTp539EgmkBC1ytCfm8z_TiMGQ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "DzKJaXf4xJ7o7BHliUv_d7wO5iEied0bB1P4GMcG2jo",
+      useTemplateId: "N6xDCv10smNIod8P1DeVL0bnQVVngz4sPmUZx3PNrIg",
       // 所在省份
       province: "北京",
       // 所在城市
